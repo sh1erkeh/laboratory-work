1a1d74f4035c18c15a2c265d3778b3a87aef31c5
sh1erkeh <arseniy29042006@gmail.com>
2024-10-10 21:36:59 +0300

    1. Final version of lab 1 2. Some fixes in preamble

--- lab-1/main.c
+++ lab-1/main.c
@@ -2,64 +2,60 @@
 #include <math.h>
 #include <stdlib.h>
 
-const MAX_N = 100;
+const int MAX_N = 100;
 
 size_t n;
-float x, h, a, *r = NULL;
+float x, h, a;
 
-int task1() {
-    printf("Input format: 'n x h a'.\n");
-    printf("Variables must be of the following types: \
-    n(positive integer), x(float), h(float), a(float).\n");
+int main() {
+    printf("Laboratory work 1\n");
+    printf("Task 1\n");
+    printf("Input format: 'n x h a'\n");
+    printf("Variables must be of the following types:\n");
+    printf("n(positive integer), x(float), h(float), a(float)\n");
 
-    int counter = 0;
     while (scanf("%zu%f%f%f", &n, &x, &h, &a) != 4 || n == 0 || n > MAX_N) {
-        if (++counter > 5) {
-            printf("Too many attempts.\n");
-            return 1;
-        }
         printf("Input format is incorrect. Try again.\n");
     }
 
-    r = (float*)calloc(n, sizeof(float));
-    if (r == NULL) {
-        printf("Memory allocation error.\n");
-        return 1;
-    }
-
+    float r[MAX_N];
     for (int i = 1; i <= n; i++) {
         r[i - 1] = 0.3 * cos(2 * a * x - i * i * h);
     }
-    return 0;
-}
 
-int task2() {
-    if (r == NULL) {
-        printf("Array is empty or not initialized.\n");
-        return 1;
+    printf("Array elements:\n");
+    for (int i = 0; i < n; i++) {
+        if (i == n - 1)
+            printf("r[%d] = %f\n", i, r[i]);
+        else
+            printf("r[%d] = %f ", i, r[i]);
     }
 
+    printf("Task 2\n");
     int last_free_pos = 0;
     for (int i = 0; i < n; i++) {
         if (abs(r[i]) > 0.7) {
             r[last_free_pos++] = r[i];
         }
     }
+
+    if (last_free_pos == 0) {
+        printf("All elements have been deleted.\n");
+        return 1;
+    } else if (last_free_pos == n) {
+        printf("Not a single element has been deleted.\n");
+    }
+
+    printf("New array of length %d:\n", last_free_pos);
+    for (int i = 0; i < last_free_pos; i++) {
+        if (i == last_free_pos - 1)
+            printf("r[%d] = %f\n", i, r[i]);
+        else
+            printf("r[%d] = %f ", i, r[i]);
+    }
     n = last_free_pos;
-    r = (float*)realloc(r, n * sizeof(float));
-    return 0;
-}
 
-int task3() {
-    if (r == NULL) {
-        printf("Array is empty or not initialized.\n");
-        return 1;
-    }
-    if (n == 1) {
-        printf("There are no elements after first maximum.\n");
-        return 1;
-    }
-
+    printf("Task 3\n");
     int first_maximum_index = 0;
     float sum = 0;
 
@@ -71,25 +67,12 @@ int task3() {
             sum += r[i];
         }
     }
-    printf("%f", sum / (n - first_maximum_index - 1));
-    return 0;
-}
 
-int main() {
-    if (task1()) {
-        printf("Task 1 failed.\n");
+    if (first_maximum_index == n - 1) {
+        printf("There are no elements after first maximum.\n");
         return 1;
     }
-    if (task2()) {
-        printf("Task 2 failed.\n");
-        return 1;
-    }
-    if (task3()) {
-        printf("Task 3 failed.\n");
-        return 1;
-    }
-    if (r != NULL) {
-        free(r);
-    }
+
+    printf("Mean value: %f", sum / (n - first_maximum_index - 1));
     return 0;
 }
